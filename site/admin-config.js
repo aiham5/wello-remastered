5f0cea731388247ef5851f0050142bafbe6ac8bf
Aiham Hamoudh <aiham5hamoudh@gmail.com>
2026-02-04 00:35:17 -0600

    Fix image preview

--- site/admin-config.js
+++ site/admin-config.js
@@ -1,4 +1,5 @@
 window.WELLO_CONFIG = {
   supabaseUrl: "https://qrohvdlntdowewhkwfub.supabase.co",
-  supabaseAnonKey: "sb_publishable_vEb9-_Yy49ZXF_8a1VP5YA_9zWfTUDx",
+  supabaseAnonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyb2h2ZGxudGRvd2V3aGt3ZnViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MzA4ODksImV4cCI6MjA4NDAwNjg4OX0.T999683JEriajF3csbPZIC0c1-q80zCvAxsk4Kb_PVs",
 };
